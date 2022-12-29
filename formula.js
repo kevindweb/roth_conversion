45ff8776c455048e378cb0f1682dee4bf17f2e8a
Kevin Deems <kevin8deems@gmail.com>
2022-12-29 13:17:36 -0500

    Add growth chart.js graph instead of text

--- formula.js
+++ formula.js
@@ -14,23 +14,109 @@ function calculate() {
     var afterTaxRateOfReturn = rateOfReturn * (1 - investmentTaxRate / 100);
     var investmentTaxes = currentTaxRate / 100 * (investmentAmount - nonDeductableAmount);
 
-    calcValues(timeHorizon, afterTaxRateOfReturn, investmentTaxes, rateOfReturn, retirementTaxRate, investmentAmount, nonDeductableAmount);
-    for (var i = 0; i <= timeHorizon; i++) {
-        calcValues(i, afterTaxRateOfReturn, investmentTaxes, rateOfReturn, retirementTaxRate, investmentAmount, nonDeductableAmount);
+    var data = {
+        labels: [],
+        datasets: [
+            {
+                label: "Traditional IRA Post-Tax",
+                backgroundColor: "green",
+                data: []
+            },
+            {
+                label: "Roth IRA",
+                backgroundColor: "blue",
+                data: []
+            },
+            {
+                label: "Traditional IRA + Tax Savings Invested",
+                backgroundColor: "red",
+                data: []
+            }
+        ]
     }
 
-    updateValue("netRothReturn", Math.floor(nROTH_TOT_ATR));
-    updateValue("netTraditionalReturn", Math.ceil(nIRA_AF_TAX_ATR));
-    updateValue("netTotalReturn", Math.ceil(nIRA_TOT_ATR));
+    if (prevChart !== undefined) {
+        prevChart.destroy();
+    }
+
+    var currentYear = new Date().getFullYear();
+    var investmentYearSet = split(currentYear, currentYear + timeHorizon, 8);
+
+    for (var i = 0; i <= timeHorizon; i++) {
+        calcValues(data, investmentYearSet, i, afterTaxRateOfReturn, investmentTaxes, rateOfReturn, retirementTaxRate, investmentAmount, nonDeductableAmount);
+    }
+
+    prevChart = new Chart(ctx, {
+        type: 'bar',
+        data: data,
+        options: {
+            tooltips: {
+                callbacks: {
+                    label: function (tooltipItem, data) {
+                        var dataSet = data.datasets[tooltipItem.datasetIndex];
+                        var value = dataSet.data[tooltipItem.index];
+                        var label = dataSet.label;
+
+                        if (parseInt(value) >= 1000) {
+                            return label + ': $' + value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                        } else {
+                            return label + ': $' + value;
+                        }
+                    }
+                }
+            },
+            barValueSpacing: 20,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        min: 0,
+                        beginAtZero: true,
+                        userCallback: function (value, index, values) {
+                            value = value.toString();
+                            value = value.split(/(?=(?:...)*$)/);
+                            value = value.join(',');
+                            return value;
+                        }
+                    }
+                }]
+            }
+        }
+    });
 }
 
-function calcValues(e, afterTaxROR, taxes, ror, taxRate, investment, deductableOffset) {
+function calcValues(chartData, chartSet, e, afterTaxROR, taxes, ror, taxRate, investment, deductableOffset) {
     nIRA_BF_TAX_ATR = investment / Math.pow(1 + ror, -1 * e);
     nIRA_TAX_ATR = (nIRA_BF_TAX_ATR - deductableOffset) * (taxRate / 100);
     nIRA_TAX_PMT_RTN_ATR = fvAmount(afterTaxROR, e, taxes);
     nIRA_AF_TAX_ATR = nIRA_BF_TAX_ATR - nIRA_TAX_ATR;
     nIRA_TOT_ATR = nIRA_AF_TAX_ATR + nIRA_TAX_PMT_RTN_ATR;
     nROTH_TOT_ATR = nIRA_BF_TAX_ATR;
+
+    var currentYear = new Date().getFullYear() + e;
+
+    if (!chartSet.has(currentYear)) {
+        return;
+    }
+
+    chartData["labels"].push(currentYear);
+    var graphDataPoints = chartData["datasets"];
+    graphDataPoints[0]["data"].push(Math.ceil(nIRA_AF_TAX_ATR));
+    graphDataPoints[1]["data"].push(Math.floor(nROTH_TOT_ATR));
+    graphDataPoints[2]["data"].push(Math.ceil(nIRA_TOT_ATR));
+}
+
+function split(left, right, parts) {
+    var result = new Set();
+    var delta = (right - left) / (parts - 1);
+    while (left < right) {
+        result.add(Math.ceil(left));
+
+        left += delta;
+    }
+
+    result.add(Math.ceil(right));
+    console.log(result);
+    return result;
 }
 
 function fvAmount(e, t, i) {
@@ -41,18 +127,8 @@ function inputValue(inputName) {
     return parseInt(document.getElementsByName(inputName)[0].value);
 }
 
-function updateValue(inputName, value) {
-    input = document.getElementById(inputName);
-    input.innerHTML = numberWithCommas(value);
-}
-
-function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-});
+var ctx = document.getElementById("investmentGainsChart").getContext("2d");
+var prevChart;
 
 // run calculation on init
 calculate();
